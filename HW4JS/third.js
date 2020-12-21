4b2bb5f66f1324e6bfe747a3191fdc1be2457d59
kedsup <v.kanaeva2012@gmail.com>
2020-12-21 16:13:53 +0200

    third

---
+++ HW4JS/third.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const input = prompt('Месяц');
+
+const map = new Map();
+
+const months = [['January', 1], ['February', 2], ['March', 3], ['April', 4], ['May', 5], ['June', 6], ['July', 7],
+  ['August', 8], ['September', 9], ['October', 10], ['November', 11], ['December', 12]];
+
+months.forEach(([string, number]) => {
+  map.set(string, number);
+  map.set(number, string);
+});
+
+const number = Number(input);
+
+const month = Number.isNaN(number) ? map.get(input) : map.get(number);
+
+if (!month) {
+  console.log('Wrong value');
+} else {
+  console.log(month);
+}
